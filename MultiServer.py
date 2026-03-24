82a81f826f21934f79115de49aa264e1941369ee
Sebastian Sanchez <sebeze2000@gmail.com>
2026-03-24 00:43:03 -0300

    mmmm

--- MultiServer.py
+++ MultiServer.py
@@ -25,7 +25,7 @@ CONFIG_GUILDS = {
         "CANAL_CARGAS_ID": 1446983648091046032,
         "CANAL_PREMIOS_ID": 1284336904095010826,
         "BACKEND_WEBHOOK": "https://gpt-kommo-bot-production.up.railway.app/webhook-ocr/crown",
-        "SHEETS_WEBHOOK_URL": "https://script.google.com/macros/s/AKfycby8-WTH3FvaJEc5HgzMg5w0yN4Sl7JBO0CR-BsqKLl2hYEcAEZcXskQvSBO99-9SHrl/exec?action=ocr"
+        "SHEETS_WEBHOOK_URL": "https://script.google.com/macros/s/AKfycbzinOPsXmlT_C_e-gRT80QRLLoDov4k1kWBHxq-E7FphNOonz3A6wivfCpmuxwAPrEV/exec?action=ocr"
     }
 }
 
